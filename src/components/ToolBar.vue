becaad733754f519bfd400fa725485efbd29327c
Gence Yalcin <genceyalcin@gmail.com>
2021-01-30 00:22:35 -0500

    Foundation

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="toolbar" class="row">
+        <div class="col-lg-3">
+            <h1>MusicBytes</h1>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class ToolBar extends Vue {
+
+}
+</script>
+
+<style>
+    #toolbar {
+        padding: 5px 15px 5px 15px;
+        box-shadow: 5px 5px 4px 4px rgba(0, 0, 0, 0.1);
+    }
+</style>
